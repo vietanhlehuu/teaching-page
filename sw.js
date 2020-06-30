56c450c3b3725c5a69ad53d14e7ec672de9daf0f
Lê Hữu Việt Anh <lehuuvietanh@gmail.com>
2020-06-30 07:40:10 +0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-12763f6a365d19e31f30.js"
+    "url": "webpack-runtime-f070ce8cecbfee852f5b.js"
   },
   {
     "url": "styles.598e0195498734c5e351.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e8bf19cf0cf90504e84983bfdc663c7d"
+    "revision": "abb0eff16fad7612f9f395e01299a2a2"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-5a1ee105f05b3144b807.js"
@@ -57,7 +57,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "84b07f3354ea8604910189579c6fdc12"
+    "revision": "f529012a35dd33f3f31760fe4a6fd600"
   },
   {
     "url": "manifest.webmanifest",
